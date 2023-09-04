9864f16986adcc9706558f35f54dd322f57e07c9
James Duff <jaduff@protonmail.com>
2023-09-04 16:59:26 +0800

    initial commit

---
+++ home-manager/home.nix
@@ -0,0 +1,111 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+
+{ inputs, outputs, lib, config, pkgs, ... }: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
+  # TODO: Set your username
+  home = {
+    username = "your-username";
+    homeDirectory = "/home/your-username";
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "jaduff";
+    userEmail = "jaduff@protonmail.com";
+  };
+  programs ={
+    fish = {
+      enable = true;
+     };
+   };
+  # Add stuff for your user as you see fit:
+  # programs.neovim.enable = true;
+  # home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [
+    vlc
+    firefox
+    lm_sensors
+    cifs-utils
+    iotop
+    inetutils
+    protonvpn-gui
+    postgresql
+    alacritty
+    remmina
+    oh-my-fish
+    fish
+    borgbackup
+    vorta
+    virt-manager
+    tmux
+    gh
+    htop-vim
+    git
+    libreoffice
+    protonvpn-gui
+    telegram-desktop
+    krita
+    inkscape
+    gimp
+    librewolf
+    nextcloud-client
+    anytype
+    joplin-desktop
+    zotero
+    zettlr
+    protonmail-bridge
+    thunderbird
+    espanso
+    skypeforlinux
+    kdeconnect
+    kate
+  ]; 
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+}
