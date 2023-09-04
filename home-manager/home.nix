6b48dc515ea221fb008d7454156015494e6407cf
James Duff <jaduff@protonmail.com>
2023-09-04 17:07:34 +0800

    added neorg flake

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,7 +6,7 @@
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
     # outputs.homeManagerModules.example
-
+    outputs.homeManagerModules.neorg
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
 
